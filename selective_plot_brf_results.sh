66aaa766a2ade5afac9b4361b73d8b9b6d4d42b9
S. Mohammad Hoseini Rad <smhr313@gmail.com>
2016-01-30 00:47:20 +0330

    Add option for legend title.

--- selective_plot_brf_results.sh
+++ selective_plot_brf_results.sh
@@ -4,12 +4,16 @@
 ## $1 is the value of converged k.
 ## $2 is the column number in the file.
 ## $3 is the type of plot: p, l or lp
+## $4 is the legend option
 
+if [ $4 = "nt" ]; then
 ## without legend
 ls brf_results/*"$1"* | sed 's/$/\"/' | sed 's/^/\"/' | sed "s/$/ u 1:"$2" t \"\" w "$3", /" | tr '\n' ' ' | sed 's/...$//' | sed 's/^/plot /' > temp.plot
 
+elif [ $4 = "t" ]; then
 ## with legend
-#ls brf_results/*"$1"* | sed 's/$/\"/' | sed 's/^/\"/' | sed "s/$/ u 1:"$2" w "$3", /" | tr '\n' ' ' | sed 's/...$//' | sed 's/^/plot /' > temp.plot
+ls brf_results/*"$1"* | sed 's/$/\"/' | sed 's/^/\"/' | sed "s/$/ u 1:"$2" w "$3", /" | tr '\n' ' ' | sed 's/...$//' | sed 's/^/plot /' > temp.plot
+fi
 
 echo "" >> temp.plot
 echo "pause -1" >> temp.plot
