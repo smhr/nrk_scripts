9ae9384a3543665fcc8796d889c05a1a2e6e3691
S. Mohammad Hoseini Rad <smhr313@gmail.com>
2016-01-29 21:05:40 +0330

    new file:   nrk_with_all_result.sh 	modified:   selective_plot_brf_results.sh

--- selective_plot_brf_results.sh
+++ selective_plot_brf_results.sh
@@ -5,7 +5,12 @@
 ## $2 is the column number in the file.
 ## $3 is the type of plot: p, l or lp
 
-ls brf_results/*_"$1"* | sed 's/$/\"/' | sed 's/^/\"/' | sed "s/$/ u 1:"$2" t \"\" w "$3", /" | tr '\n' ' ' | sed 's/...$//' | sed 's/^/plot /' > temp.plot
+## without legend
+ls brf_results/*"$1"* | sed 's/$/\"/' | sed 's/^/\"/' | sed "s/$/ u 1:"$2" t \"\" w "$3", /" | tr '\n' ' ' | sed 's/...$//' | sed 's/^/plot /' > temp.plot
+
+## with legend
+#ls brf_results/*"$1"* | sed 's/$/\"/' | sed 's/^/\"/' | sed "s/$/ u 1:"$2" w "$3", /" | tr '\n' ' ' | sed 's/...$//' | sed 's/^/plot /' > temp.plot
+
 echo "" >> temp.plot
 echo "pause -1" >> temp.plot
 gnuplot temp.plot
